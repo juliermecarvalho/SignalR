7cf838898215e4b6141faf5a0f7037c56900a1d0
Julierme carvalho <Juliermecarvalho@gmail.com>
2013-08-21 10:48:13 -0300

    commit

---
+++ SignalRExemplo01/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SignalRExemplo01.MvcApplication" Language="C#" %>
---
+++ SignalRExemplo01/Hubs/Chat.cs
@@ -0,0 +1,13 @@
+﻿using SignalR.Hubs;
+
+
+namespace SignalRExemplo01.Hubs
+{
+    public class Chat : Hub
+    {
+        public void EnviarMensagem(string apelido, string mensagem)
+        {
+            Clients.TransmitirMensagem(apelido, mensagem);
+        }
+    }
+}
